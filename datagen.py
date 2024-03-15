8acc14fe7890f055d85c87a43d16bdde5c0deef3
brianpzaide <brianpzaide@gmail.com>
2024-03-15 20:51:11 +0530

    feat: removing duplicate code from datagen.py

--- datagen.py
+++ datagen.py
@@ -73,97 +73,10 @@ def generate_data():
     conn.commit()
     conn.close()
 
-def fetch_notes(job_application_id):
-    conn = sqlite3.connect('jbt.db')
-    cur = conn.cursor()
-    notes = cur.execute(notes_fetch_query, (job_application_id,)).fetchall()
-    conn.close()
-    print(type(notes))
-    print(notes)
-    print(len(notes))
-    for _note in notes:
-        print(_note, end="\n==\n")
-    return notes
-
-def fetch_job_application(job_application_id: int)->JobApplication:
-    conn = sqlite3.connect('jbt.db')
-    conn.row_factory = sqlite3.Row
-    cur = conn.cursor()
-    job_application = JobApplication(**cur.execute(job_application_fetch_query, (job_application_id,)).fetchone())
-    conn.close()
-    print(job_application)
-    return job_application
-
-def update_job_application(job_application_id:int, new_status: str, next_date: str, next_time: str):
-    conn = sqlite3.connect('jbt.db')
-    cur = conn.cursor()
-    cur.execute(job_update_query, (new_status, f"{next_date} {next_time}", job_application_id,))
-    conn.commit()
-    conn.close()
-
-def add_note(job_application_id:int, new_note:str):
-    conn = sqlite3.connect('jbt.db')
-    cur = conn.cursor()
-    cur.execute(notes_insert_query, (job_application_id, new_note,))
-    conn.commit()
-    conn.close()
-
-def filter_job_applications(companies: List[str]=None, positions: List[str]=None, status:List[str]=None, _from:str="", _through: str="" )->List[JobApplication]:
-    conn = sqlite3.connect('jbt.db')
-    conn.row_factory = sqlite3.Row
-    cur = conn.cursor()
-    _params = ()
-
-    result_query = "Select * from job_applications where "
-    if companies is not None and len(companies) > 0:
-        result_query += f"company in ({','.join(['?']*len(companies))}) "
-        _params = _params + tuple(companies)
-    if positions is not None and len(positions) > 0:
-        result_query += f"and position in ({','.join(['?']*len(positions))}) "
-        _params = _params + tuple(positions)
-    if status is not None and len(status) > 0:
-        result_query += f"and status in ({','.join(['?']*len(status))}) "
-        _params = _params + tuple(status)
-    if _from:
-        result_query += "and next_deadline >= datetime(?) "
-        _params = _params + (_from,)
-    if _through:
-        result_query += "and next_deadline <= datetime(?)" 
-        _params = _params + (_through,)
-
-    results = cur.execute(result_query, _params).fetchall()
-
-    filtered_job_applications: List[JobApplication] = []
-    
-    for res in results:
-        filtered_job_applications.append(JobApplication(**res))
-
-    for j in filtered_job_applications:
-        print(j)
-
-    conn.close()
-
-
 if __name__ == '__main__':
-    # init_db()
+    init_db()
     generate_data()
-    
-    # fetch_notes(152)
-    
-    # fetch_job_application(152)
-    
-    # update_job_application(job_application_id=152, new_status="Applied", next_date="2024-04-07", next_time="10:00")
-    # fetch_job_application(152)
 
-    # add_note(152, lorem_ipsum)
-    # fetch_notes(152)
-
-    # filter_job_applications(companies=["autodesk", "reef technologies", "hasura", "citadel", "2 sigma"],
-    #     positions=["technical writer", "devops", "infra"],
-    #     status = ["ready to apply", "ghosted", "interview scheduled", "rejected", "archived"],
-    #     _from = "2024-02-28",
-    #     _through = "2024-04-15",
-    # )
 
 
 
